d196bc8fe42b83615e966fd88c4a0a327e323770
namke-dev <namke.dev@gmail.com>
2023-12-30 02:50:39 +0700

    add some style

--- src/components/ImageGenerator/ImageGenerator.jsx
+++ src/components/ImageGenerator/ImageGenerator.jsx
@@ -1,22 +1,21 @@
 import React, { useRef, useState } from "react";
 import "./ImageGenerator.css";
-import place_hodler_img from "../Assets/default_img.jpg";
-import fetchImageFromApi from "../../lib/fetch-image-from-api";
+import { fetchImageFromApi } from "../../lib/fetch-image-from-api";
 
-const ImageGenerator = () => {
+export default function ImageGenerator() {
   const [img_url, setImg_url] = useState("/");
   let inputRef = useRef(null);
   const [loading, setloading] = useState(false);
 
   const imageGenerator = async () => {
-    if (inputRef.current.value === "") {
+    if (!inputRef) {
       return;
     }
     setloading(true);
-
     let response = null;
+
     try {
-      inputRef && (response = await fetchImageFromApi(inputRef));
+      response = await fetchImageFromApi({ inputRef });
       let data = await response.json();
       let data_array = data.data;
       if (data_array) {
@@ -29,41 +28,50 @@ const ImageGenerator = () => {
     } finally {
       setloading(false);
     }
+  };
+
+  const handleSubmitForm = (event) => {
+    if (event.key === "Enter" && !loading) {
+      imageGenerator();
+    }
+  };
 
   return (
     <div className="ai-image-generator">
-      <div className="header">
-        AI Image <span> generator</span>
+      <div className="header !text-gray-50">
+        Image generator <span>OpenAI </span>
       </div>
 
       <div className="image-loading">
         <div className="image">
           <img
-            src={img_url === "/" ? place_hodler_img : img_url}
+            src={img_url === "/" ? "default_img.jpg" : img_url}
             alt=""
             className=""
           />
         </div>
         <div className="loading">
           <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
-          <div className={loading ? "loading-text" : "display-none"}>
-            Loading...
-          </div>
+          <div className={loading ? "loading-text" : "display-none"}></div>
         </div>
       </div>
       <div className="search-box">
         <input
           ref={inputRef}
-          className="search-input"
+          className={`search-input ${loading ? "disabled" : ""}`}
           placeholder="Describe What you Want To See"
           type="text"
+          onKeyDown={handleSubmitForm}
+          disabled={loading}
         />
-        <div onClick={() => imageGenerator()} className="generate-btn">
-          Go
-        </div>
+        <button
+          onClick={() => imageGenerator()}
+          className={`generate-btn ${loading ? "disabled" : ""}`}
+          disabled={loading}
+        >
+          GO
+        </button>
       </div>
     </div>
   );
-};
-
-export default ImageGenerator;
+}
